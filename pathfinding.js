c574e2dad7b64934e5395384b86a427a231350bc
Noah Ramdial <noahramdial57@gmail.com>
2021-07-31 22:13:53 -0400

    Find path added. A * needs work.

--- pathfinding.js
+++ pathfinding.js
@@ -13,11 +13,11 @@ class PriorityQueue {
 
   enqueue(element, priority) {
     // creating object from queue element
-    var qElement = new QElement(element, priority);
-    var contain = false;
+    let qElement = new QElement(element, priority);
+    let contain = false;
 
     // iterating through the entire item array to add element at the correct location of the Queue
-    for (var i = 0; i < this.items.length; i++) {
+    for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].priority > qElement.priority) {
         // Once the correct location is found it is enqueued
         this.items.splice(i, 0, qElement);
@@ -42,8 +42,21 @@ class PriorityQueue {
     // return true if the queue is empty.
     return this.items.length == 0;
   }
+  front() {
+    // returns the highest priority element in the Priority queue without removing it.
+    if (this.isEmpty())
+      return "No elements in Queue";
+    return this.items[0];
+  }
+  rear() {
+    // returns the lowest priority element of the queue
+    if (this.isEmpty())
+      return "No elements in Queue";
+    return this.items[this.items.length - 1];
+  }
 }
 
+let myMap = new Map();
 // Creating the grid 
 function grid() {
   let gridContainer = document.querySelector(".grid");
@@ -61,6 +74,12 @@ function grid() {
       box.className = "box";
       box.id = k + '-' + i;
       row.appendChild(box);
+      let loc = {
+        x: k,
+        y: i,
+        parent: null
+      }
+      myMap.set(box, loc)
     }
     container.appendChild(row);
   }
@@ -153,9 +172,10 @@ function clearPath() {
       }
     }
   }
+  setCoor()
 }
 
-async function generateRandomObstacles(delay = 10) {
+async function generateRandomObstacles(delay = 7) {
   clearGrid()
   for (i = 0; i < 11; i++) {
     for (j = 0; j < 33; j++) {
@@ -232,40 +252,70 @@ function exploreLocation(location) {
   return allNeighbors;
 }
 
-// does not work || its running time is too slow
-async function BFS(delay = 0) {
-  let start = document.querySelector('.box-start').id
+async function findPath(delay = 50) {
   let end = document.querySelector('.box-end').id
   let endCoor = [getXaxis(end), getYaxis(end)]
+  let a = myMap.get(getNodeId(endCoor[0], endCoor[1])) // works
+  let path = []
+  setCoor()
 
+  for (i = 0; i < 100; i++) {
+    let parent = a.parent
+    if (parent == null) {
+      break
+    } else {
+      path.push(parent)
+    }
+    a = myMap.get(parent)
+    await new Promise(resolve =>
+      setTimeout(() => {
+        resolve();
+      }, delay)
+    );
+    if (path[i].classList == 'box box-start') {
+      break
+    } else {
+      path[i].className = 'box box-path'
+    }
+  }
+}
+
+// It works || running time is faster now
+async function BFS(delay = 0) {
+  let start = document.querySelector('.box-start').id
+  let end = document.querySelector('.box-end')
   let location = {
     x: getXaxis(start),
     y: getYaxis(start),
   }
 
   let queue = [];
-  let counter = 0
   queue.push(location);
   while (queue.length) {
-    let currentLocation = queue[counter];
-    if (currentLocation.x == endCoor[0] && currentLocation.y == endCoor[1])
-      return currentLocation;
-    // else mark the node as visited
-    let node = getNodeId(currentLocation.x, currentLocation.y)
-    node.className = 'box box-visited';
-    await new Promise(resolve =>
-      setTimeout(() => {
-        resolve();
-      }, delay)
-    );
-    console.log(queue.length)
-    counter = counter + 1
+    let currentLocation = queue.shift();
     let neighbors = exploreLocation(currentLocation);
+
     for (neighbor of neighbors) {
-      if (neighbor.node.classList != "box box-visited") {
+      if (neighbor.node == end) {
+        location = getNodeId(neighbor.x, neighbor.y)
+        let a = myMap.get(location)
+        let node = getNodeId(currentLocation.x, currentLocation.y)
+        a.parent = node;
+        return findPath()
+
+      } else if (neighbor.node.classList != "box box-visited") {
         queue.push(neighbor);
-        currentLocation = getNodeId(neighbor.x, neighbor.y)
-        currentLocation.className = 'box box-visited';
+        location = getNodeId(neighbor.x, neighbor.y)
+        let a = myMap.get(location)
+        let node = getNodeId(currentLocation.x, currentLocation.y)
+        a.parent = node;
+        location.className = 'box box-visited';
+
+        await new Promise(resolve =>
+          setTimeout(() => {
+            resolve();
+          }, delay)
+        );
       }
     }
   }
@@ -301,7 +351,7 @@ async function DFS(delay = 25) {
       start.className = 'box box-start'
       let neighbors = exploreLocation(node);
       for (neighbor of neighbors) {
-        if (node.classList != ".box-visited" || node.classList != ".box-wall") {
+        if (node.classList != ".box-visited") {
           stack.push(neighbor);
         }
       }
@@ -309,13 +359,11 @@ async function DFS(delay = 25) {
   }
 }
 
-/*
-function Djikstra() {
-  let start = document.querySelector('.box-start')
-  let end = document.querySelector('.box-end')
-  let startId = start.id // string
-  let endId = end.id // string
-  let endCoor = [getXaxis(endId), getYaxis(endId)]
+
+function Dijkstra() {
+  let start = document.querySelector('.box-start').id
+  let end = document.querySelector('.box-end').id
+  let endCoor = [getXaxis(end), getYaxis(end)]
 
   let distances = {};
 
@@ -333,24 +381,86 @@ function Djikstra() {
       } else if (node.classList = '.box-wall') {
         continue
       } else {
-        distances[node] = 1
+        distances[node] = Infinity
       }
-      prev[node] = null;
-
-
-      while (!pq.isEmpty()) {
-        let minNode = pq.dequeue();
-        let currNode = minNode.data;
-        let weight = minNode.priority;
-        this.edges[currNode].forEach(neighbor => {
-          let alt = distances[currNode] + neighbor.weight;
-          if (alt < distances[neighbor.node]) {
-            distances[neighbor.node] = alt;
-            prev[neighbor.node] = currNode;
-            pq.enqueue(neighbor.node, distances[neighbor.node]);
-          }
-        });
-      }
-      return distances;
     }
-    */
+  }
+
+  while (!pq.isEmpty()) {
+    let minNode = pq.dequeue();
+    let currNode = minNode.data;
+    let weight = minNode.priority;
+    // explore neighbors
+    let neighbors = exploreLocation()
+    for (neighbor of neighbors) {
+      let alt = distances[currNode] + neighbor.weight;
+      if (alt < distances[neighbor.node]) {
+        distances[neighbor.node] = alt;
+        prev[neighbor.node] = currNode;
+        pq.enqueue(neighbor.node, distances[neighbor.node]);
+      }
+    }
+  }
+  return distances;
+}
+
+// The heuristic function that returns the distance between two points
+function manhattanDistance(p1, p2) {
+  let x1 = getXaxis(p1)
+  let y1 = getYaxis(p1)
+  let x2 = getXaxis(p2)
+  let y2 = getYaxis(p2)
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2)
+}
+
+
+// f(n) = g(n) + h(n) 
+function Astar() {
+  let start = document.querySelector('.box-start').id
+  let end = document.querySelector('.box-end').id
+  let endCoor = [getXaxis(end), getYaxis(end)]
+  let distances = PriorityQueue(); //This contains the distances from the start node to all other nodes
+  distances.enqueue(start, 0); //The distance from the start node to itself is of course 0
+  let priorities = PriorityQueue(); //This contains the priorities with which to visit the nodes, calculated using the heuristic.
+
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 60; j++) {
+      priorities.enqueue(getNodeId(j, i)) = Number.MAX_VALUE //Initializing with a priority of "Infinity"
+      distances.enqueue(getNodeId(j, i)) = Number.MAX_VALUE //Initializing with a distance of "Infinity"
+    }
+  }
+
+  //start node has a priority equal to straight line distance to goal. It will be the first to be expanded.
+  priorities.enqueue(start, manhattanDistance(start, end))
+
+  //While there are nodes left to visit...
+  while (true) {
+    // find the node with the currently lowest priority going through all nodes that haven't been visited yet
+    let node = priorities.rear()
+    if (node.classList != 'box box-visited' && node.classList == 'box box-end') {
+      return console.log('we found the end')
+    }
+
+    //...then, for all neighboring nodes that haven't been visited yet....
+    let curr = node.id
+    let location = {
+      x: getXaxis(curr),
+      y: getYaxis(curr),
+    }
+
+    let neighbors = exploreLocation(location)
+    for (neighbor of neighbors) {
+      if (neighbor.node.classList != 'box-box-visited') {
+        //...if the path over this edge is shorter...
+        if (distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i] < distances[i]) {
+          //...save this path as new shortest path
+          distances[i] = distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i];
+          //...and set the priority with which we should continue with this node
+          priorities[i] = distances[i] + heuristic[i][goal];
+          // console.log("Updating distance of node " + i + " to " + distances[i] + " and priority to " + priorities[i]);
+        }
+      }
+    }
+    visited[lowestPriorityIndex] = true;
+  }
+}
