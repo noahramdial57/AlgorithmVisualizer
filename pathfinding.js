8c8aac028e38992f69f7111a9c4bdfa14176f1a2
Noah Ramdial <noahramdial57@gmail.com>
2021-08-02 14:46:23 -0400

    A* completed.

--- pathfinding.js
+++ pathfinding.js
@@ -48,11 +48,28 @@ class PriorityQueue {
       return "No elements in Queue";
     return this.items[0];
   }
+  has(element) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (element == this.items[i]) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
   rear() {
     // returns the lowest priority element of the queue
     if (this.isEmpty())
       return "No elements in Queue";
-    return this.items[this.items.length - 1];
+    let rear = this.items[this.items.length - 1]
+    this.items.length = this.items.length - 1
+    return rear;
+  }
+  printPQueue() {
+    var str = "";
+    for (var i = 0; i < this.items.length; i++)
+      str += this.items[i].element + " ";
+    return str;
   }
 }
 
@@ -77,7 +94,10 @@ function grid() {
       let loc = {
         x: k,
         y: i,
-        parent: null
+        parent: null,
+        fScore: Number.MAX_VALUE,
+        gScore: Number.MAX_VALUE,
+        aStarParent: null
       }
       myMap.set(box, loc)
     }
@@ -175,13 +195,13 @@ function clearPath() {
   setCoor()
 }
 
-async function generateRandomObstacles(delay = 7) {
+async function generateRandomObstacles(delay = 0) {
   clearGrid()
   for (i = 0; i < 11; i++) {
     for (j = 0; j < 33; j++) {
       let randX = Math.floor(Math.random() * 60);
       let randY = Math.floor(Math.random() * 20);
-      node = getNodeId(randX, randY)
+      let node = getNodeId(randX, randY)
       if (node.classList == 'box box-start' || node.classList == 'box box-end') {
         continue
       } else {
@@ -194,6 +214,29 @@ async function generateRandomObstacles(delay = 7) {
       );
     }
   }
+}
+
+/*
+function randomizeNodePos() {
+  wipeBoard()
+  let startRandX = Math.floor(Math.random() * 59);
+  let startRandY = Math.floor(Math.random() * 19);
+  let startNode = getNodeId(startRandX, startRandY)
+  let endRandX = Math.floor(Math.random() * 59);
+  let endRandY = Math.floor(Math.random() * 19);
+  let endNode = getNodeId(endRandX, endRandY)
+
+  startNode.className = 'box box-start'
+  endNode.className = 'box box-end'
+}
+*/
+
+function draggableNodes() {
+  let start = document.querySelector('.box-start').id
+  let end = document.querySelector('.box-end')
+  node.addEventListener("ondragstart", function () {
+    node.className = 'box box-wall'
+  });
 }
 
 // BREADTH FIRST SEARCH || Depth First Search || Djiksta || A *
@@ -259,8 +302,36 @@ async function findPath(delay = 50) {
   let path = []
   setCoor()
 
-  for (i = 0; i < 100; i++) {
-    let parent = a.parent
+  for (i = 0; i < 1000; i++) {
+    parent = a.parent
+    if (parent == null) {
+      break
+    } else {
+      path.push(parent)
+    }
+    a = myMap.get(parent)
+    await new Promise(resolve =>
+      setTimeout(() => {
+        resolve();
+      }, delay)
+    );
+    if (path[i].classList == 'box box-start') {
+      break
+    } else {
+      path[i].className = 'box box-path'
+    }
+  }
+}
+
+async function aStarFindPath(delay = 50) {
+  let end = document.querySelector('.box-end').id
+  let endCoor = [getXaxis(end), getYaxis(end)]
+  let a = myMap.get(getNodeId(endCoor[0], endCoor[1])) // works
+  let path = []
+  setCoor()
+
+  for (i = 0; i < 1000; i++) {
+    parent = a.aStarParent
     if (parent == null) {
       break
     } else {
@@ -288,13 +359,14 @@ async function BFS(delay = 0) {
     x: getXaxis(start),
     y: getYaxis(start),
   }
-
   let queue = [];
   queue.push(location);
-  while (queue.length) {
-    let currentLocation = queue.shift();
-    let neighbors = exploreLocation(currentLocation);
+  let counter = 0
 
+  while (queue.length) {
+    let currentLocation = queue[counter];
+    counter = counter + 1
+    let neighbors = exploreLocation(currentLocation);
     for (neighbor of neighbors) {
       if (neighbor.node == end) {
         location = getNodeId(neighbor.x, neighbor.y)
@@ -319,6 +391,7 @@ async function BFS(delay = 0) {
       }
     }
   }
+  queue = []
   return false;
 }
 
@@ -413,54 +486,54 @@ function manhattanDistance(p1, p2) {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2)
 }
 
-
 // f(n) = g(n) + h(n) 
-function Astar() {
-  let start = document.querySelector('.box-start').id
-  let end = document.querySelector('.box-end').id
-  let endCoor = [getXaxis(end), getYaxis(end)]
-  let distances = PriorityQueue(); //This contains the distances from the start node to all other nodes
-  distances.enqueue(start, 0); //The distance from the start node to itself is of course 0
-  let priorities = PriorityQueue(); //This contains the priorities with which to visit the nodes, calculated using the heuristic.
+// it works
+async function aStar(delay = 0) {
+  let start = document.querySelector('.box-start')
+  let end = document.querySelector('.box-end')
+  let open = new PriorityQueue();
+  let init = myMap.get(start)
+  init.gScore = 0
+  init.fScore = manhattanDistance(start.id, end.id)
+  open.enqueue(start, init.fScore);
 
-  for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 60; j++) {
-      priorities.enqueue(getNodeId(j, i)) = Number.MAX_VALUE //Initializing with a priority of "Infinity"
-      distances.enqueue(getNodeId(j, i)) = Number.MAX_VALUE //Initializing with a distance of "Infinity"
-    }
-  }
-
-  //start node has a priority equal to straight line distance to goal. It will be the first to be expanded.
-  priorities.enqueue(start, manhattanDistance(start, end))
-
-  //While there are nodes left to visit...
-  while (true) {
-    // find the node with the currently lowest priority going through all nodes that haven't been visited yet
-    let node = priorities.rear()
-    if (node.classList != 'box box-visited' && node.classList == 'box box-end') {
+  //While there are nodes left to visit
+  while (!open.isEmpty()) {
+    // find the node with the currently lowest priority
+    let currentNode = open.dequeue().element    
+    if (currentNode.classList == 'box box-end') {
+      aStarFindPath()
       return console.log('we found the end')
     }
 
-    //...then, for all neighboring nodes that haven't been visited yet....
-    let curr = node.id
+    let curr = currentNode.id
     let location = {
       x: getXaxis(curr),
       y: getYaxis(curr),
     }
 
+    currentNode.className = 'box box-visited'
+    await new Promise(resolve =>
+      setTimeout(() => {
+        resolve();
+      }, delay)
+    );
+      
     let neighbors = exploreLocation(location)
     for (neighbor of neighbors) {
-      if (neighbor.node.classList != 'box-box-visited') {
-        //...if the path over this edge is shorter...
-        if (distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i] < distances[i]) {
-          //...save this path as new shortest path
-          distances[i] = distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i];
-          //...and set the priority with which we should continue with this node
-          priorities[i] = distances[i] + heuristic[i][goal];
-          // console.log("Updating distance of node " + i + " to " + distances[i] + " and priority to " + priorities[i]);
+      let temp_gScore = myMap.get(currentNode).gScore + manhattanDistance(currentNode.id, neighbor.node.id)
+      let neighbor_gScore = myMap.get(neighbor.node).gScore
+      if (temp_gScore < neighbor_gScore) {
+        myMap.get(neighbor.node).aStarParent = currentNode
+        myMap.get(neighbor.node).gScore = temp_gScore
+        myMap.get(neighbor.node).fScore = myMap.get(neighbor.node).gScore + manhattanDistance(neighbor.node.id, end.id)
+        if (!open.has(neighbor.node)) {
+          open.enqueue(neighbor.node, myMap.get(neighbor.node).fScore)
         }
       }
     }
-    visited[lowestPriorityIndex] = true;
   }
+  return false
 }
+
+
